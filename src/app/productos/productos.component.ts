7cbf22b69c17cdb4a773ab519f50a0eff87a7613
Jesneth <114118307+Jesneth@users.noreply.github.com>
2022-11-27 21:25:14 -0500

    Initial commit

---
+++ src/app/productos/productos.component.ts
@@ -0,0 +1,10 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'productos-app',
+  templateUrl: './productos.component.html',
+  styleUrls: ['./productos.component.css'],
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+}
